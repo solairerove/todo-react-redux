c4f0b470c092ef201e4e25d642d60efb54c33bc9
solairerove <hax2033@gmail.com>
2017-05-09 13:40:21 +0300

    Split simple todo mvc example

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {applyMiddleware, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+import App from './containers/App';
+import rootReducer from './reducers';
+import 'todomvc-app-css/index.css'
+
+import './index.css';
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(logger)
+);
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
